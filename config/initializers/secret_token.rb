a611738e4ff71b43bd16b25c8addc3166fd80caa
Christian Reyes <cjreyes@andrew.cmu.edu>
2011-09-21 19:09:51 -0400

    initial commit of management app. previous folder contents put into /files

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Knight::Application.config.secret_token = 'a824461710ab0e5241b5eb40b01bb6849839650eae5eb597353f5c9e9515228524493d9df3919580a688f73a54e9f67aa5537a47e6be62123c5596bfeee7c2b3'
