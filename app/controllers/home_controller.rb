90fabdfd6118fa5453af869f25e3bea5015ccb65
Christian Reyes <cjreyes@andrew.cmu.edu>
2011-09-22 03:45:22 -0400

    better layout. checks unit tests before cucumber

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,13 +1,21 @@
 class HomeController < ApplicationController
   def grade
-	Dir.chdir("repositories/#{params[:reponame]}") do
+	repo_path = "repositories/#{params[:reponame]}"
+	Dir.chdir(repo_path) do
 		if Dir.entries(Dir.pwd).include?("app") && Dir.entries(Dir.pwd).include?("public")
 			# rails application
-			@output = `cucumber`	
+			@output_unit = `rake test:units`
+			
+			@info = /(\d+) tests, (\d+) assertions, (\d+) failures, (\d+) errors, (\d+) skips/.match(@output_unit)
+			
+			if @info[3] == "0" && @info[4] == "0"
+				@output_cucumber = `cucumber`
+			end
+			
 		else
 			# unit test example
-			@output = `ruby -Itest credit_cards_test.rb`	
-			@info = /(\d+) tests, (\d+) assertions, (\d+) failures, (\d+) errors, (\d+) skips/.match(@output)
+			@output_unit = `ruby -Itest credit_cards_test.rb`	
+			@info = /(\d+) tests, (\d+) assertions, (\d+) failures, (\d+) errors, (\d+) skips/.match(@output_unit)
 		end
 	end
   end
